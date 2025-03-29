bf480d880a4319652b3d1084fa1c5ed1e8575541
duncte123 <contact@duncte123.me>
2025-03-29 11:26:47 +0100

    UWU

--- src/graphics/unread-donations/main.vue
+++ src/graphics/unread-donations/main.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { donationsToRead } from '@esa-layouts/browser_shared/replicant_store';
+import Donation from './components/Donation.vue';
+</script>
+
 <template>
   <div>
     <h1>Unread Donations</h1>
@@ -7,7 +12,7 @@
     >
       None right now!
     </div>
-    <donation
+    <Donation
       v-for="(donation, index) in donationsToRead"
       :key="donation.id"
       :data="donation"
@@ -15,22 +20,6 @@
     />
   </div>
 </template>
-
-<script lang="ts">
-import { replicantNS } from '@esa-layouts/browser_shared/replicant_store';
-import { DonationsToRead } from '@esa-layouts/types/schemas';
-import { Component, Vue } from 'vue-property-decorator';
-import Donation from './components/Donation.vue';
-
-@Component({
-  components: {
-    Donation,
-  },
-})
-export default class extends Vue {
-  @replicantNS.State((s) => s.reps.donationsToRead) readonly donationsToRead!: DonationsToRead;
-}
-</script>
 
 <style>
   body {
