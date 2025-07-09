651d63ad519add35bf62e5f87e02cae9e35265cc
Duncan Sterken <contact@duncte123.me>
2025-07-09 10:22:19 +0200

    Add special layouts for annual (#29)
    * Convert 16-9 layout
    
    * 16x9-2p
    
    * 4x3-1p
    
    * Better gradient
    
    * 4x3-2p and a lot of bug fixes to make it slightly compatible with the bsg styling
    
    * Allign estimate

---
+++ src/graphics/game-layout/uksg/16x9-1p.vue
@@ -0,0 +1,136 @@
+<script setup lang="ts">
+import MediaBoxBox from '../components/MediaBoxBox.vue';
+import ParticipantInfo from './components/ParticipantInfo.vue';
+import RunInfo from './components/RunInfo.vue';
+import TimerElem from './components/TimerElem.vue';
+import GameCapture from '../components/GameCapture.vue';
+</script>
+
+<template>
+  <div>
+    <!-- Camera Feed -->
+    <div
+      id="CameraCapture1"
+      class="Fixed Capture BorderRight BorderBottom"
+      :class="$style.CameraFeed"
+    />
+    <!-- Runner/Player -->
+    <ParticipantInfo
+      :class="$style.Player"
+      header-font-size="18px"
+      type="player"
+    />
+    <!-- Host -->
+    <ParticipantInfo
+      :class="$style.Host"
+      type="host"
+      header-font-size="14px"
+      header-width="66px"
+      name-font-size="20px"
+      cut-edge-size="8px"
+    />
+    <!-- Comm1 -->
+    <ParticipantInfo
+      :class="$style.Comm1"
+      type="comm1"
+      header-font-size="14px"
+      header-width="66px"
+      name-font-size="20px"
+      cut-edge-size="8px"
+    />
+    <!-- Comm2 -->
+    <ParticipantInfo
+      :class="$style.Comm2"
+      type="comm2"
+      header-font-size="14px"
+      header-width="66px"
+      name-font-size="20px"
+      cut-edge-size="8px"
+    />
+    <div class="Fixed" :class="$style.ImageRotation">
+      <MediaBoxBox
+        style="position: unset;"
+        :class="$style.ImageRotation"
+      />
+    </div>
+    <!-- Game Feed -->
+    <GameCapture
+      id="GameCapture1"
+      class="Fixed Capture BorderLeft BorderBottom"
+      :class="$style.GameFeed"
+      :slot-no="0"
+    />
+    <TimerElem
+      class="BorderLeft BorderTop"
+      :class="$style.Timer"
+      vertical
+      timer-size="80px"
+    />
+    <RunInfo :class="$style.RunInfo" />
+  </div>
+</template>
+
+<style lang="scss" module>
+.CameraFeed {
+  left: 0px;
+  top: 0px;
+  width: 480px;
+  height: 369px;
+}
+
+.Player {
+  left: 0px;
+  top: 366px;
+  width: 480px;
+  height: 81px;
+}
+
+.Host {
+  left: 0px;
+  top: 444px;
+  width: 480px;
+  height: 54px;
+}
+
+.Comm1 {
+  left: 0px;
+  top: 495px;
+  width: 480px;
+  height: 54px;
+}
+
+.Comm2 {
+  left: 0px;
+  top: 546px;
+  width: 480px;
+  height: 54px;
+}
+
+.ImageRotation {
+  left: 0px;
+  top: 600px;
+  width: 477px;
+  height: 400px;
+}
+
+.GameFeed {
+  left: 477px;
+  top: 0px;
+  width: 1443px;
+  height: 814px;
+}
+
+.Timer {
+  left: 477px;
+  top: 811px;
+  width: 706px;
+  height: 189px;
+}
+
+.RunInfo {
+  left: 1183px;
+  top: 811px;
+  width: 737px;
+  height: 192px;
+}
+</style>
