2283e9cb3fce6563f260b6e6148f531db6b5b644
duncte123 <contact@duncte123.me>
2025-08-31 16:22:10 +0200

    Draw first layout in css

--- src/graphics/game-layout/16x9-1p.vue
+++ src/graphics/game-layout/16x9-1p.vue
@@ -6,13 +6,15 @@ import RunInfo from './components/RunInfo.vue';
 import Timer from './components/Timer.vue';
 import FlashingLightsWarning from './components/FlashingLightsWarning.vue';
 import MediaBoxBox from './components/MediaBoxBox.vue';
+import CommentatorsList from './components/CommentatorsList.vue';
 </script>
 
 <template>
-  <div class="bsglayout _16x9_1p">
+  <div class="bsglayout">
     <!-- Game Captures -->
     <GameCapture
       id="GameCapture1"
+      class="BorderNone"
       :style="{
         left: '483px',
         top: '0px',
@@ -21,82 +23,98 @@ import MediaBoxBox from './components/MediaBoxBox.vue';
       }"
     />
 
-    <!-- Camera Captures -->
     <div
-      id="CameraCapture1"
-      class="Capture Relative"
+      class="Flex Fixed BorderTop"
       :style="{
-        left: '0px',
-        top: '0px',
-        width: '462px',
-        height: '349px',
-      }"
-    />
-
-    <div
-      class="Fixed FlexColumn"
-      :style="{
-      left: '0px',
-      top: '361px',
-      width: '469px',
-      height: '44px',
+        left: '474px',
+        top: '822px',
+        width: '1445px',
+        height: '176px',
       }"
     >
-      <Player/>
-    </div>
-
-    <!-- Run Game Info -->
-    <div
-      class="Fixed FlexColumn"
-      :style="{
+      <!-- Timer -->
+      <div
+        class="Fixed FlexColumn BorderRight"
+        :style="{
           left: '474px',
-          top: '822px',
+          top: '827px',
           width: '731px',
-          height: '176px',
+          height: '171px',
         }"
-    >
-      <Timer
-        class="TimerFH"
-        line-right
-        line-left
-        :line-bottom="false"
-      />
-    </div>
+      >
+        <Timer
+          class="TimerFH"
+          line-right
+          line-left
+          :line-bottom="false"
+        />
+      </div>
 
-    <!-- Run Game Info -->
-    <div
-      class="Fixed FlexColumn"
-      :style="{
+      <!-- Run Game Info -->
+      <div
+        class="Fixed FlexColumn"
+        :style="{
         left: '1210px',
         top: '822px',
         width: '710px',
         height: '176px',
       }"
-    >
-      <RunInfo text-align="left" info-is-row />
+      >
+        <RunInfo text-align="left" info-is-row/>
+      </div>
     </div>
 
-    <!-- Media Box / Reader / Comms -->
-    <div
-      class="Fixed FlexColumn"
-      :style="{
+    <div class="Flex BorderRight" :style="{
+      height: '100%',
+      width: '469px',
+    }">
+      <!-- Camera Captures -->
+      <div
+        id="CameraCapture1"
+        class="Capture Relative"
+        :style="{
+        left: '0px',
+        top: '0px',
+        width: '462px',
+        height: '349px',
+      }"
+      />
+
+      <div
+        class="Fixed FlexColumn BorderTop BorderBottom"
+        :style="{
+          left: '0px',
+          top: '361px',
+          width: '469px',
+          height: '44px',
+        }">
+        <Player/>
+      </div>
+
+      <!-- Media Box / Reader / Comms -->
+      <div
+        class="Fixed FlexColumn"
+        :style="{
         left: '0px',
         top: '410px',
         width: '469px',
         height: '588px',
       }"
-    >
-      <FlashingLightsWarning />
-      <MediaBoxBox
-        vertical
-        line-right
-        :style="{
+      >
+        <CommentatorsList class="BorderTop BorderBottom" />
+        <!-- <CommentatorsReader class="BorderTop BorderBottom"/> -->
+        <CommentatorsReader class="BorderBottom" show-reader/>
+        <FlashingLightsWarning/>
+        <MediaBoxBox
+          vertical
+          line-right
+          class="BorderTop1"
+          :style="{
           width: '469px',
           height: '100%',
         }"
-      />
-      <CommentatorsReader line-top show-reader />
-      <CommentatorsReader line-top />
+        />
+      </div>
     </div>
   </div>
 </template>
