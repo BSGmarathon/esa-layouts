3d2bbe201de7e7f18401ea283e26d4adced414ac
duncte123 <contact@duncte123.me>
2025-09-14 14:40:39 +0200

    Convert 3ds 1p vertical

--- src/graphics/game-layout/3dsv-1p.vue
+++ src/graphics/game-layout/3dsv-1p.vue
@@ -9,86 +9,98 @@ import FlashingLightsWarning from './components/FlashingLightsWarning.vue';
 </script>
 
 <template>
-  <div class="bsglayout _3ds_1p_vertical">
+  <div class="bsglayout">
     <!-- Run Game Info -->
     <div
-      class="Fixed FlexColumn"
+      class="Fixed FlexColumn BorderBottom BorderRight"
       :style="{
-      left: '0x',
-      top: '0px',
-      width: '574px',
-      height: '252px',
+        left: '0x',
+        top: '0px',
+        width: '574px',
+        height: '252px',
       }"
     >
-      <RunInfo />
-    </div>
-
-    <!-- Host/comms -->
-    <div
-      class="Fixed FlexColumn"
-      :style="{
-      left: '0x',
-      top: '257px',
-      width: '574px',
-      height: '97px',
-      }"
-    >
-      <CommentatorsReader show-reader />
-      <CommentatorsReader />
+      <RunInfo line-right />
     </div>
 
     <!-- Game Captures -->
     <GameCapture
       id="GameCapture1"
+      class="BorderBottom BorderLeft BorderRight"
       :style="{
-      left: '846px',
-      top: '0px',
-      width: '808px',
-      height: '486px',
+        left: '790px',
+        top: '0px',
+        width: '907px',
+        height: '510px',
+      }"
+    />
+
+    <div
+      class="Fixed BorderBottom"
+      :style="{
+        left: '574px',
+        top: '0px',
+        width: '100%',
+        height: '510px',
       }"
     />
 
     <GameCapture
       id="GameCapture3"
+      class="BorderLeft BorderRight"
       :style="{
-      left: '928px',
-      top: '513px',
-      width: '646px',
-      height: '485px',
+        left: '925px',
+        top: '515px',
+        width: '644px',
+        height: '483px',
       }"
     />
 
     <!-- Camera Captures -->
     <div
       id="CameraCapture1"
-      class="Capture Relative"
+      class="Capture Relative BorderRight"
       :style="{
-      left: '0px',
-      top: '363px',
-      width: '566px',
-      height: '307px',
+        left: '0px',
+        top: '257px',
+        width: '574px',
+        height: '307px',
       }"
     />
 
     <!-- Player -->
     <div
-      class="Fixed"
+      class="Fixed BorderRight BorderTop"
       :style="{
-      left: '0px',
-      top: '683px',
-      width: '574px',
-      height: '44px',
+        left: '0px',
+        top: '560px',
+        width: '574px',
+        height: '44px',
       }"
     >
       <Player/>
     </div>
 
+    <!-- Host/comms -->
+    <div
+      class="Fixed FlexColumn BorderRight"
+      :style="{
+        left: '0x',
+        top: '609px',
+        width: '574px',
+        height: '97px',
+      }"
+    >
+      <CommentatorsReader class="BorderTop" />
+      <CommentatorsReader class="BorderTop" show-reader />
+    </div>
+
     <!--TIMER-->
     <div
-      class="Fixed FlexColumn"
+      class="Fixed FlexColumn BorderTop BorderRight"
       :style="{
         left: '0px',
-        top: '732px',
+        top: '706px',
         width: '574px',
         height: '106px',
       }"
@@ -98,12 +110,12 @@ import FlashingLightsWarning from './components/FlashingLightsWarning.vue';
 
     <!-- Media Box -->
     <div
-      class="Fixed FlexColumn"
+      class="Fixed FlexColumn BorderTop BorderRight"
       :style="{
         left: '0px',
-        top: '838px',
+        top: '811px',
         width: '574px',
-        height: '160px',
+        height: '182px',
       }"
     >
       <FlashingLightsWarning class="Flex" style="align-self: flex-end" />
